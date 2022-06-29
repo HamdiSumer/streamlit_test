55f358401cac14d0ee7758f1e2a3ed05af3173d3
HamdiSumer <hamdismer@hotmail.com>
2022-06-29 12:04:28 +0300

    test

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
 
-st.write(
-    """
-         
-    Hello testing again!
-    """
+st.title("Strength Prediction")
+
+st.header("Configurations:", anchor=None)
+
+st.subheader("Feature Database:")
+
+st.markdown("Select which features to use while training the model")
+
+options = st.multiselect(
+    "What are your favorite colors",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"],
 )
+
+st.write("You selected:", options)
