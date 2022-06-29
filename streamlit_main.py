e6ffdbf218de22cb194002ba4d2768da09ca9053
HamdiSumer <hamdismer@hotmail.com>
2022-06-29 13:32:56 +0300

    d

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,83 +1,15 @@
 import streamlit as st
 
-
 st.title("Strength Prediction")
 
-st.header("Configurations:", anchor=None)
+plant = st.selectbox(
+     'Select Plant',
+     ('Büyükçekmece Fabrikası', 'Çanakkale Fabrikası', 'Karçimsa Fabrikası', 'Ladik Fabrikası'))
 
-st.subheader("Feature Database:")
+date_initial = st.date_input(
+     "From:",
+     datetime.date(2019, 7, 6))
 
-options = st.multiselect(
-    "Select which features to use while training the model (Do not remove 2 - 7 - 28 day strengths!",
-    [
-        "K2O",
-        "Al2O3",
-        "Freelime",
-        "SO3",
-        "Fe2O3",
-        "Na2O",
-        "Le Chatelier",
-        "Cl",
-        "45mm",
-        "Insoluble Residue",
-        "Water Demand",
-        "Initial Setting Time",
-        "Specific Gravity",
-        "CaO",
-        "LOI",
-        "MgO",
-        "Blaine",
-        "SiO2",
-        "Type",
-        "S.M",
-        "Total",
-        "Date of Sample",
-        "EQ",
-        "32mm",
-        "90mm",
-        "C3A",
-        "C2S",
-        "C3S",
-        "H.M",
-        "Al.M",
-        "LSF",
-        "L.faz",
-        "2days",
-        "7days",
-        "28days",
-    ],
-    [
-        "K2O",
-        "Al2O3",
-        "Freelime",
-        "SO3",
-        "Fe2O3",
-        "Na2O",
-        "Le Chatelier",
-        "Cl",
-        "45mm",
-        "Insoluble Residue",
-        "Water Demand",
-        "Initial Setting Time",
-        "Specific Gravity",
-        "CaO",
-        "LOI",
-        "MgO",
-        "Blaine",
-        "SiO2",
-        "Type",
-        "2days",
-        "7days",
-        "28days",
-    ],
-)
-
-choice = st.radio("Model:", ["LightGBM", "XGBoost", "Neural Network"])
-
-normalize_check = st.checkbox("Normalize Features?")
-
-onehotencode_check = st.checkbox("One Hot Encode Cement Type?")
-
-
-st.subheader("Upload Dataset to Predict:")
-# st.write("You selected:", options)
+date_initial = st.date_input(
+     "To:",
+     datetime.date(2019, 7, 6))
