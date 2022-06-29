a8e891b8af39e8a3ee23925820fa7fc9eb1b0378
HamdiSumer <hamdismer@hotmail.com>
2022-06-29 12:29:48 +0300

    test

--- streamlit_main.py
+++ streamlit_main.py
@@ -8,13 +8,14 @@ st.header("Configurations:", anchor=None)
 st.subheader("Feature Database:")
 
 options = st.multiselect(
-    "Select which features to use while training the model",
+    "Select which features to use while training the model (Do not remove 2 - 7 - 28 day strengths!",
     [
         "K2O",
         "Al2O3",
         "Freelime",
         "SO3",
-        "Fe2O3" "Na2O",
+        "Fe2O3",
+        "Na2O",
         "Le Chatelier",
         "Cl",
         "45mm",
@@ -33,20 +34,25 @@ options = st.multiselect(
         "Date of Sample",
         "EQ",
         "32mm",
-        "90mm" "C3A",
+        "90mm",
+        "C3A",
         "C2S",
         "C3S",
         "H.M",
         "Al.M",
         "LSF",
         "L.faz",
+        "2days",
+        "7days",
+        "28days",
     ],
     [
         "K2O",
         "Al2O3",
         "Freelime",
         "SO3",
-        "Fe2O3" "Na2O",
+        "Fe2O3",
+        "Na2O",
         "Le Chatelier",
         "Cl",
         "45mm",
@@ -60,7 +66,18 @@ options = st.multiselect(
         "Blaine",
         "SiO2",
         "Type",
+        "2days",
+        "7days",
+        "28days",
     ],
 )
 
+choice = st.radio("Model:", ["LightGBM", "XGBoost", "Neural Network"])
+
+normalize_check = st.checkbox("Normalize Features?")
+
+onehotencode_check = st.checkbox("One Hot Encode Cement Type?")
+
+
+st.subheader("Upload Dataset to Predict:")
 # st.write("You selected:", options)
